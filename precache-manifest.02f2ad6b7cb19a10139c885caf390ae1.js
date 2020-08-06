d06255369df29609bb024849123aac71783d333f
Rohit <kadamrohit0101@gmail.com>
2020-08-06 23:54:50 +0530

    Updates

---
+++ precache-manifest.02f2ad6b7cb19a10139c885caf390ae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae24bfb1177921be7b5f03a6fe5add12",
+    "url": "/Meme-Generator/index.html"
+  },
+  {
+    "revision": "c51eca34c0cf7acffc64",
+    "url": "/Meme-Generator/static/css/main.2148bf7c.chunk.css"
+  },
+  {
+    "revision": "29ea514b03c1dfc7a337",
+    "url": "/Meme-Generator/static/js/2.809d901d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Meme-Generator/static/js/2.809d901d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c51eca34c0cf7acffc64",
+    "url": "/Meme-Generator/static/js/main.5fa9a861.chunk.js"
+  },
+  {
+    "revision": "f8100a08c460be6d4954",
+    "url": "/Meme-Generator/static/js/runtime-main.b5c2cf97.js"
+  }
+]);
